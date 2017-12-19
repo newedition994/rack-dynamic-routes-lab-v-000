cad579a58c145ca6fdce9fc7fb13593b14fbab69
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-19 21:52:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item = @@items.find { |i| i.name == item_name  }
-        
+      if item = @@items.find {|i| i.name == item_name}
+
       end
     end
 
